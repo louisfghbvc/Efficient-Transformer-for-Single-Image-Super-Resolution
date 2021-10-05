67e4c1a7852066c2386392ec413148f6280f7db2
NawaNae <t105590029@ntut.edu.tw>
2021-10-06 01:35:30 +0800

    train, valid, load, save, scheduler done

--- scripts/efficientTransformerSR.py
+++ scripts/efficientTransformerSR.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-
+import math
 import torch
 from torch import optim
 from tqdm import tqdm
+
 
 from torch.utils.data import DataLoader
 from models.models import ESRT
@@ -11,6 +12,7 @@ from .configParser import ConfigParser
 
 class EfficientTransformerSR:
     def __init__(self, configs= "train"):
+        title("Initialize")
         self.configs = None
         self.epoch = None
         self.initConfigs(configs)
@@ -34,12 +36,26 @@ class EfficientTransformerSR:
         self.endEpoch = end
         for epoch in range(start, end):    
             self.epoch = epoch
-            self.epochAction("train", self.trainloader)
-            self.epochAction("valid", self.validloader)
-            
-    def forwardAction(self, x, y):
+            trainLoss, tranCorrect = self.epochAction("train", self.trainloader)
+            self.trainLosses.append(trainLoss.item())
+            if (epoch + 1) % self.configs["saveEvery"] == 0:
+                self.save()
+
+            validLoss, validCorrect = self.epochAction("valid", self.validloader)
+            self.validLosses.append(validLoss.item())
+            self.learningRates.append(self.learningRate)
+            if validLoss < self.bestValidLoss:
+                self.bestValidLoss = validLoss
+                [best.unlink() for best in getFiles(self.getCheckpointFolder(),"best*.pth")] # remove last best pth
+                self.save(f"bestEpoch{epoch+1}.pth")
+                info(f"save best model, valid loss {round(validLoss.item(),3)}")
+            self.scheduler.step(validLoss)
+    @property
+    def learningRate(self):
+        return self.optimizer.param_groups[0]['lr'] 
+    
+    def modelForward(self, x, y):
         device = self.device
-        if DEBUG: return torch.Tensor([[1]]), torch.Tensor([0])
         x, y = x.to(device), y.to(device)
         out = self.model(x)
         loss = self.criterion(out, y)
@@ -48,42 +64,49 @@ class EfficientTransformerSR:
     def epochAction(self, action, loader):
         isBackward = True if action == "train" else False
         GradSelection = Grad if isBackward else torch.no_grad
-        totalLoss, totalCorrect = 0, 0
+        totalLoss, totalCorrect, totalLen = 0, 0, 0
         batchLoader = tqdm(loader)
         with GradSelection():
             for x, y in batchLoader:
                 self.optimizer.zero_grad()
-                out, loss = self.forwardAction(x,y)
-                if not DEBUG:
-                    totalLoss += loss 
-                    totalCorrect += torch.sum(y == out)
-                    if isBackward:
-                        loss.backward()
-                        self.optimizer.step()
+                out, loss = self.modelForward(x,y)
+
+                totalLoss += loss 
+                totalCorrect += torch.sum(y == out)
+                totalLen += len(x)
+                if isBackward:
+                    loss.backward()
+                    self.optimizer.step()
                 epochProgress = f"{self.epoch+1}/{self.configs['epochs']}" if action != "test" else "1/1"
-                
-                batchLoader.set_description(desc=f"[{epochProgress}] {action} loss : {round(loss.item() / len(y),2)} ")
-    
+                batchLoader.set_description(desc=f"{action} [{epochProgress}] -lglr {'%.1f'%(-math.log(self.learningRate,10))} 🕐loss {'%.2f'%(loss.item() / len(y))} ➗loss {'%.2f'%(totalLoss/totalLen)}")
+        return totalLoss/len(batchLoader), totalCorrect/len(batchLoader)
     def train(self, loader = None):
+        title("Train")
         self.trainloader = loader or self.trainloader
         self.load()
         self.trainEpochs( self.startEpoch, self.configs["epochs"])
-    def valid(self, loader = None):
-        loader = loader or self.validloader
-        self.epochAction("valid",loader)
+
     def test(self):
+        title("Test")
+        self.loadBest()
+        #(TODO) Write Loader here
+        self.epochAction("test",...)
         ...
     def saveObject(self, epoch):
         return {
             "epoch" : epoch,
             "model" : self.model.state_dict(),
             "scheduler" : self.scheduler.state_dict(),
-            "optimizer" : self.optimizer.state_dict()
+            "optimizer" : self.optimizer.state_dict(),
+            "trainLosses" : self.trainLosses,
+            "validLosses" : self.validLosses,
+            "learningRates" : self.learningRates
         }
     def getCheckpointFolder(self):
         return PATHS.CHECKPOINTS / f"ETSR-lr{ self.configs['startLearningRate'] }-flip{self.configs['randomFlip']}-psize{self.configs['patchSize']}" 
-    def save(self,fileName, epoch=1):
-        fileName = fileName or f"epoch{epoch}.pth"
+    def save(self,fileName = ""):
+        epoch = self.epoch
+        fileName = fileName or f"epoch{epoch+1}.pth"
         saveFolder = self.getCheckpointFolder()
         mkdir(saveFolder)
         fileName = saveFolder / fileName
@@ -97,23 +120,38 @@ class EfficientTransformerSR:
         if startEpoch == 0:
             return #if 0 no load (including can't find )
         
-        modelFile = getFile(saveFolder, "epoch{startEpoch}")
-        saveObject = torch.load(modelFile)
+        modelFile = getFile(saveFolder, f"epoch{startEpoch}.pth")
+        self.loadParams(modelFile)
+    def loadBest(self):
+        modelFile = getFile(self.getCheckpointFolder(), "best*.pth")
+        if modelFile:
+            self.loadParams(modelFile)
+        else:
+            warn(f"best model not found under {self.getCheckpointFolder()}\nIs 'bestXXX.pth' exist?")
+            self.load()
+    def loadParams(self,fileP):
+        info(f"load model from {fileP.name}")
+        saveObject = torch.load(fileP)
         self.model.load_state_dict(saveObject["model"])
         self.scheduler.load_state_dict(saveObject["scheduler"])
-        self.optimizer.load_stat_dict(saveObject["optimizer"])
-        
-    def loadParams(self):
-        ...
+        self.optimizer.load_state_dict(saveObject["optimizer"])
+        self.validLosses = saveObject["validLosses"]
+        self.trainLosses = saveObject["trainLosses"]
+        self.learningRates = saveObject["learningRates"]
+        self.bestValidLoss = max([*self.validLosses,0])
     def initParams(self):
         self.criterion = torch.nn.L1Loss()
         self.model = ESRT()
         self.model = self.model.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.configs["startLearningRate"])
         self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer )
+        self.trainLosses = []
+        self.validLosses = []
+        self.learningRates = []
+        self.bestValidLoss = float("inf")
         #(TODO) : implement loader 
-        self.trainloader = [(f"train x{i}","y{i}") for i in range(1000)] 
-        self.validloader = [(f"valid x{i}","y{i}") for i in range(500)]
+        self.trainloader = torch.tensor([[[0.],[0.]] for i in range(1000)]) 
+        self.validloader = torch.tensor([[[1.],[1.]] for i in range(500)])
 
 
 if __name__ == '__main__':
