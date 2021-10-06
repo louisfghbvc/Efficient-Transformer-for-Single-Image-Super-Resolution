1f48fa982a9f65b8ad30a3ec1a53daa7b13e9d17
yehcheshin <shin109062639@gapp.nthu.edu.tw>
2021-10-06 18:24:04 +0800

    finish "ARFB"

--- models/ARFB.py
+++ models/ARFB.py
@@ -2,13 +2,6 @@ import torch
 import torch.nn as nn
 from torch.nn.modules import module
 
-# class AFRB(nn.Module):
-#     def __init__(self):
-#         super(AFRB,self).__init__()
-#             pass
-    
-#     def forward(self,x):
-#         pass
 
 
 def defaultConv(inChannels, outChannels, kernelSize, bias=True):
@@ -19,13 +12,13 @@ def defaultConv(inChannels, outChannels, kernelSize, bias=True):
 
 
 class ResidualUnit(nn.Module):
-    def __init__(self,nFeats):
+    def __init__(self, inChannel, outChannel, reScale,kernelSize=1, bias=True):
         super(ResidualUnit,self).__init__()
 
-        self.reduction = defaultConv(nFeats,nFeats//2,3)
-        self.expansion = defaultConv(nFeats//2,nFeats,3)
-        self.lamRes = 0.5
-        self.lamX= 0.5
+        self.reduction = defaultConv(inChannel, outChannel//2, kernelSize, bias)
+        self.expansion = defaultConv(outChannel//2, inChannel, kernelSize, bias)
+        self.lamRes = reScale.lamRes
+        self.lamX= reScale.lamX
     
 
     def forward(self,x):
@@ -35,19 +28,36 @@ class ResidualUnit(nn.Module):
         
         return  x
 
-# class ARHM(nn.Module):
-#     def __init__(self,nFeats):
-#         super(ARHM,self).__init__()
-    
-#         RU = [ResidualUnit(nFeats) for _ in range(2)]
-#         self.RU = nn.Sequential(*RU)
-
-if __name__=="__main__":
+class ARFB(nn.Module):
+    def __init__(self, inChannel, outChannel, reScale):
+        super(ARFB,self).__init__()
+        self.RU = ResidualUnit(inChannel, outChannel, reScale) 
+        self.conv1 = defaultConv(2*inChannel, 2*outChannel, kernelSize=1)
+        self.conv3 = defaultConv(2*inChannel, outChannel, kernelSize=3)
+        self.reScale = reScale
+    def forward(self,x):
+        
+        x_ru1 = self.RU(x)
+        x_ru2 = self.RU(x_ru1)
+        x_ru  = torch.cat((x_ru1,x_ru2),1)
+        x_ru  = self.conv1(x_ru)
+        x_ru  = self.conv3(x_ru)
+        x_ru  = self.reScale.lamRes*x_ru
+        x     = x*self.reScale.lamX + x_ru
+        return  x
+        
+        
+class Config():
+    lamRes=0.5
+    lamX = 0.5
+if __name__== "__main__":
     # RU = ResidualUnit(nFeats=4)
-    x = torch.tensor([float(i+1) for i in range(64)]).reshape((1, 4, 4, 4))
-
-    RU = ResidualUnit(x.shape[1])
-    res = RU(x)
+    x = torch.tensor([float(i+1) for i in range(128)]).reshape((1, -1,4, 4))
+    reScale = Config()
+    arfb = ARFB(x.shape[1], x.shape[1],reScale)
+    res = arfb(x)
+    # RU = ResidualUnit(x.shape[1])
+    # res = RU(x)
     
 
-_
+
